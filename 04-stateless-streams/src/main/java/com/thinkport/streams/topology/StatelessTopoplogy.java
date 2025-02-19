e229374cec194ed5b60a257c0b352840b09390c0
ppolster <ppolster@thinkport.digital>
2025-02-19 04:14:56 +0100

    add 03 and 04

---
+++ 04-stateless-streams/src/main/java/com/thinkport/streams/topology/StatelessTopoplogy.java
@@ -0,0 +1,53 @@
+package com.thinkport.streams.topology;
+
+import com.thinkport.producer.model.ClickJson;
+import com.thinkport.streams.config.CustomSerdes;
+import digital.thinkport.avro.ClickAvro;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.support.serializer.JsonSerde;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+@Component
+@Slf4j
+public class StatelessTopoplogy {
+    @Value("${spring.kafka.properties.schema.registry.url}")
+    private String schemaRegistryUrl;
+
+    @Value("${kafka-topics.clicks-in}")
+    private String clicksTopicIn;
+
+    @Value("${kafka-topics.clicks-out}")
+    private String clicksTopicOut;
+
+    @Bean
+    public KStream<String, ClickJson> storeStream(StreamsBuilder kStreamBuilder) {
+        Properties properties = new Properties();
+        properties.put("schema.registry.url",schemaRegistryUrl);
+        KStream<String,ClickJson> stream = kStreamBuilder.stream(clicksTopicIn, Consumed.with(Serdes.String(), new JsonSerde<>(ClickJson.class)));
+        stream.peek((k,v)->LOG.info("Key: " + k + ", Value: " + v ))
+                .mapValues(clickJson-> {
+                    return ClickAvro.newBuilder()
+                            .setClickId(clickJson.getClickId())
+                            .setBytes(clickJson.getBytes())
+                            .setIp(clickJson.getIp())
+                            .setKnownIp(clickJson.isKnownIp())
+                            .setUserId(clickJson.getUserId())
+                            .setProductId(clickJson.getProductId())
+                            .setReferrer(clickJson.getReferrer())
+                            .setStatus(clickJson.getStatus())
+                            .setRequest(clickJson.getRequest())
+                            .setUserAgent(clickJson.getUserAgent())
+                            .build();
+                }).to("clicksTopicOut", Produced.with(Serdes.String(), CustomSerdes.getClickSerde(properties)));
+         return stream;
+    }
+}
