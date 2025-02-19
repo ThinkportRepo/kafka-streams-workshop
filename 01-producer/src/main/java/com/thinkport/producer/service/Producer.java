245245ff86ae7042d53d2d97c75dd408fdf6b90a
ppolster <ppolster@thinkport.digital>
2025-02-19 12:54:04 +0100

    prepare labs

--- 01-producer/src/main/java/com/thinkport/producer/service/Producer.java
+++ 01-producer/src/main/java/com/thinkport/producer/service/Producer.java
@@ -6,73 +6,43 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.datafaker.Faker;
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.header.Header;
 import org.apache.kafka.common.header.internals.RecordHeader;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
 @Slf4j
 public class Producer {
+  public Producer(KafkaTemplate<String, String> template){
+      this.template = template;
+  }
+  @Value("${spring.kafka.bootstrap-servers}")
+  private String bootstrapServer;
+
   private static final String TOPIC = "my-first-topic";
   private final KafkaTemplate<String, String> template;
   private final Faker faker = new Faker();
 
-  @Scheduled(fixedRate = 10000)
-
+  //@Scheduled(fixedRate = 10000)
   public void sendPlainJava() {
-    String bootstrapServers = "localhost:9092";
 
     // create Producer properties
     Properties properties = new Properties();
-    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-    properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-    properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
     KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-    ArrayList<Header> headers = new ArrayList<>();
-    headers.add(new RecordHeader("traceId", faker.idNumber().valid().getBytes(StandardCharsets.UTF_8)));
-
-    ProducerRecord<String, String> producerRecord =
-            new ProducerRecord<>(TOPIC, null,"key",faker.elderScrolls().city(),headers);
-    producer.send(producerRecord, (recordMetadata, e) -> {
-      // executes every time a record is successfully sent or an exception is thrown
-      if (e == null) {
-        // the record was successfully sent
-        log.info("Received new metadata. \n" +
-                "Topic:" + recordMetadata.topic() + "\n" +
-                "Partition: " + recordMetadata.partition() + "\n" +
-                "Offset: " + recordMetadata.offset() + "\n" +
-                "Timestamp: " + recordMetadata.timestamp());
-      } else {
-        log.error("Error while producing", e);
-      }
-    });
   }
 
   @Scheduled(fixedRate = 10000)
   public void sendSpringBoot() {
-    ArrayList<Header> headers = new ArrayList<>();
-    headers.add(new RecordHeader("traceId", faker.idNumber().valid().getBytes(StandardCharsets.UTF_8)));
-
-    ProducerRecord<String, String> producerRecord =
-            new ProducerRecord<>(TOPIC, null,"key",faker.elderScrolls().city(),headers);
-    try {
-      template.send(producerRecord).get(10, TimeUnit.SECONDS);
-      log.info("Sent record with Spring Boot template.");
-    } catch (InterruptedException e) {
-      Thread.currentThread().interrupt();
-      log.info("Failed to wait for record send.", e);
-    } catch (Exception e) {
-      log.info("Failed to send record.", e);
-    }
-
+      //template.send("producerRecord").get(10, TimeUnit.SECONDS);
   }
 }
